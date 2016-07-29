fa6c741cb4114711ab77425bea64d3ac6e6bf876
Tami <catquarks@gmail.com>
2016-07-29 19:42:38 -0400

    made email parser big mystery! why does the bang operator result in a nil result on an array that's just been flattened in place???

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -1,4 +1,28 @@
-# Build a class EmailParser that accepts a string of unformatted 
-# emails. The parse method on the class should separate them into
-# unique email addresses. The delimiters to support are commas (',')
-# or whitespace (' ').
+require 'pry'
+class EmailParser
+
+	attr_accessor :emails
+	attr_reader :parsed_emails
+
+	def initialize(*emails)
+		@emails = emails
+		# @parsed_emails = []
+	end
+
+	def parse
+		@emails.collect {|email| email.split(/,\s|\s/)}.flatten.uniq
+
+		# @emails.collect {|email| email.split(/,\s|\s/)}.flatten.uniq! #=> why didn't this one work??? why does the ! result in nil??
+
+
+		### this one worked until i tried using the ! but why???
+		# @emails.each {|email| @parsed_emails << email.split(/,\s|\s/)}
+		# @parsed_emails.flatten.uniq
+	end
+
+end
+
+emails = "john@doe.com, person@somewhere.org"
+parser = EmailParser.new(emails)
+ 
+parser.parse
